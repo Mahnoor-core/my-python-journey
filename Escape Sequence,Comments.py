97c9094bc3612e2a28798eb1fd73b2557d1881dc
buildwith-Mahnoor <0.mahnoor.54@gmail.com>
2025-06-14 16:05:47 +0500

    update file

---
+++ Escape Sequence,Comments.py
@@ -0,0 +1,20 @@
+
+print("Hello World
+Welcome")
+                     # It will throw error
+                     # if we want a new line we can use Escape Sequence
+                    
+
+print("Hello World \nWelcome")
+                     # Here \n is Escape Sequence for nxt line
+
+
+
+print("How to add comments in python?")
+
+# Comments means the lines that we dont want to execute but we write them for our understanding or to convey any msg
+# We can add single line comment or multiple line comments 
+
+# For single line we just use '#' before our text
+# For multiple lines we use triple ''' or """ before the comment as well as after the comment 
+
--- print_function.py
+++ print_function.py
@@ -1,6 +1,6 @@
 print("Hello World")    # print is a function to display text or values in console 
 
-print(7)                    # We don't need double quotes " "
+print(7)                    # We don't need double quotes here " "
 print("Welcome")            # But for a string we need it " "
 print("Time",3)             # We can use multiple values
 print(2+2)                  # We can also calculate

531a691e5a90da4ed1f0d81ac879d7fa9615b62c
Maha <0.mahnoor.54@gmail.com>
2025-06-15 15:23:57 +0500

    Update 01_Print.py

--- 01_Print.py
+++ 01_Print.py
@@ -16,10 +16,3 @@ print("Hello")
         # Default sep is space
         # For end, by default there is new line
 =======
-print("Hey", 6, 7, sep="~")  # Separator: Specify how to separate the objs, if there are more than one objs
-print("Hey", 6, 7, end="000")  # End: Specify what to print at the end
-
-# sep and end are optional
-# Default sep is space
-# For end, by default there is new line
->>>>>>> 62bf82e305cb3f7abebadd3e43f59c15c5141943

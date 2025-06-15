de336a7e4a70860f4f40d8cb127aa550d42fcef4
Maha <0.mahnoor.54@gmail.com>
2025-06-15 15:25:51 +0500

    Update 01_Print.py

--- 01_Print.py
+++ 01_Print.py
@@ -8,11 +8,10 @@ print(2 + 2)                        # We can also calculate
 
 # Some more about print statement
 
-<<<<<<< HEAD
 print("Hey", 6, 7, sep="~")         # Separator: Specify how to separate the objs, if there are more than one objs
 print("Hey", 6, 7, end="000")       # End: Specify what to print at the end
 print("Hello")
         # sep and end are optional
         # Default sep is space
         # For end, by default there is new line
-=======
+

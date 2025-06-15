d84c9b38919fbe1e6a14cd805cb11dc576a0eaa6
buildwith-Mahnoor <0.mahnoor.54@gmail.com>
2025-06-15 15:20:44 +0500

    Update 03.Variable_Comments

---
+++ 03_Variables_DataTypes.py
@@ -0,0 +1,25 @@
+# Variable is like a container that holds data
+
+a = 1
+print(a)        # Here a is avariable to store 1
+b = "Hello"
+print(b)        # b is a variable to store Hello
+c = True
+print(c)        # c is a variable to store True
+
+
+'''-------------'''
+
+
+# Data type specifies the type of value that a variable holds
+'''
+    1. Numeric data: int, float, complex
+    2. Text data: str, char
+    3. Boolean data: consist of true and false
+'''
+
+# type() is used to show the data type of any varaible
+
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))

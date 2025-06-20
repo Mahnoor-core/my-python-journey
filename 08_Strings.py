4bef4d5e0063b776e3dd7abed77a37d91263f9b3
buildwith-Mahnoor <0.mahnoor.54@gmail.com>
2025-06-20 10:49:19 +0500

    updated 08_file

--- 08_Strings.py
+++ 08_Strings.py
@@ -4,6 +4,9 @@ name = "Mahnoor"
 name = 'Maha'       # Note: Strings can be inclosed in single or double quotes
 print(name)
 
+'''---------'''
+
+
 # To write multipe line string, we can use triple quotes
 
 greetings = """Hi Maha, 
@@ -11,14 +14,20 @@ Welcome...
 How are you?"""
 print(greetings)
 
+"""---------"""
+
+
 # In python, strings is like an array of characters
-# We can access parts of strings by using index 
+# We can access parts of strings by using index
 
 print(name[0])
 print(name[1])
 print(name[2])
 print(name[3])
 # print(name[4])    Throws an error
+
+"""---------"""
+
 
 # We can also access all these characters using loops
 

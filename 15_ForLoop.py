72848c70095306fb1e9940471802f4d413fb3732
buildwith-Mahnoor <0.mahnoor.54@gmail.com>
2025-06-30 12:50:14 +0500

    Updated 15_file

---
+++ 15_ForLoop.py
@@ -0,0 +1,34 @@
+# Sometimes a programmer wants to execute a group of statements a certain number of times
+# This can be done using loops
+
+
+# For loop syntax
+name = "Maha"
+for i in name:
+    print(i)
+
+print("-------")
+
+
+# What is range()?
+for i in range(4):
+    print(i)
+
+print("-------")
+
+
+# What if we want to print from 2
+for j in range(2,5):
+    print(j)
+
+print("-------")
+
+
+# What if we want even numbers
+for k in range(2, 11, 2):
+    print(k)            # The third argument skips each second number
+
+print("-------")
+
+
+# We can use any type of command in for statement

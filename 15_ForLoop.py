56d5a64bb869bcbb33c979ee04e6dae9e83dfd63
buildwith-Mahnoor <0.mahnoor.54@gmail.com>
2025-06-30 12:51:59 +0500

    Updated 15_file

--- 15_ForLoop.py
+++ 15_ForLoop.py
@@ -6,28 +6,24 @@
 name = "Maha"
 for i in name:
     print(i)
-
 print("-------")
 
 
 # What is range()?
 for i in range(4):
     print(i)
-
 print("-------")
 
 
 # What if we want to print from 2
 for j in range(2,5):
     print(j)
-
 print("-------")
 
 
 # What if we want even numbers
 for k in range(2, 11, 2):
     print(k)            # The third argument skips each second number
-
 print("-------")
 
 

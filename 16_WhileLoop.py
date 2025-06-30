dd95c004424dd2378e57c9b18b9fde31bd01e235
buildwith-Mahnoor <0.mahnoor.54@gmail.com>
2025-06-30 13:11:46 +0500

    16_file

---
+++ 16_WhileLoop.py
@@ -0,0 +1,14 @@
+# While loop execute statement while the condition in the loop is True
+
+# While loop syntax
+i = 3
+while(i<6):
+    print(i)
+    i = i + 1
+print("------")
+
+j = 5
+while(j>0):
+    print(j)
+    j = j - 1
+print("------")

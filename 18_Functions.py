964b0fe4d0ca45d10e669fa77e71641be73e9fef
buildwith-Mahnoor <0.mahnoor.54@gmail.com>
2025-07-01 15:22:57 +0500

    updated 18_file

---
+++ 18_Functions.py
@@ -0,0 +1,14 @@
+# A function is a block of code that performs a specific task whenever it is called
+
+# ADD Function
+def add(a,b):
+    sum = a + b
+    print(sum)      # This is a block of function which executes every time we call it
+
+c = 7
+d = 4
+add(c, d)
+
+e = 5
+f = 95
+add(e, f)
